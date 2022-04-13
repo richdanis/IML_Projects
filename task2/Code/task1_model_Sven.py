0dfeadfef7711d9105223c4d5a709915a939fe82
Sven Gutjahr <sven.gutjahr@yahoo.de>
2022-04-13 19:35:36 +0200

    19351304

--- task2/Code/task1_model_Sven.py
+++ task2/Code/task1_model_Sven.py
@@ -4,8 +4,8 @@ from sklearn.linear_model import SGDClassifier
 from sklearn.model_selection import cross_val_score
 from sklearn.feature_selection import SelectKBest
 
-TEST1 = ['LABEL_BaseExcess', 'LABEL_Fibrinogen', 'LABEL_AST', 'LABEL_Alkalinephos', 'LABEL_Bilirubin_total', 'LABEL_Lactate',
-         'LABEL_TroponinI', 'LABEL_SaO2', 'LABEL_Bilirubin_direct', 'LABEL_EtCO2']
+TEST1 = ['LABEL_BaseExcess', 'LABEL_Fibrinogen', 'LABEL_AST', 'LABEL_Alkalinephos', 'LABEL_Bilirubin_total',
+         'LABEL_Lactate', 'LABEL_TroponinI', 'LABEL_SaO2', 'LABEL_Bilirubin_direct', 'LABEL_EtCO2']
 
 def remove_outliers(features,labels):
     for col in features.columns:
@@ -27,62 +27,51 @@ def remove_outliers(features,labels):
 
     return features, labels
 
-def prepare_dataset(df, drop):
-
-    if not drop == None:
-        df = df.drop(columns=drop)
+def prepare_dataset(df):
+    df = df.drop('pids')
     df = (df - df.mean()) / df.std()
 
     X = df.to_numpy()
-
-    # exclude pids
-    X = X[:, 1:]
-    # exclude ages
-    X = X[:, 1:]
-    X = X.reshape((len(age), 12 * X.shape[-1]))
-    X = np.hstack((age, X))
-
     return X
 
 fname = 'Data/'
-train_df = pd.read_csv(fname + 'data_interpolated_means.csv')
-label_df = pd.read_csv(fname + 'train_labels.csv')
-test_df = pd.read_csv(fname + 'testdata_means_long.csv')
+df_train = pd.read_csv(fname + 'train_features_Sven_short.csv')
+df_label = pd.read_csv(fname + 'train_labels_sorted.csv')
+df_test = pd.read_csv(fname + 'test_features_Sven_short.csv')
 
-X_train = prepare_dataset(train_df, 'Time')
-T_test = prepare_dataset(test_df, None)
+X_train = prepare_dataset(df_train)
+T_test = prepare_dataset(df_test)
 
 # PREPARING TRAINING LABELS
-label_df = label_df.sort_values(by=['pid'])
-label_df = label_df[TEST1]
+df_label = df_label.sort_values(by=['pid'])
+df_label = df_label[TEST1]
 
-y = label_df.to_numpy()
-y = y[:,1:]
+y_train = df_label.to_numpy()
 
-output = np.empty((T.shape[0],10))
+output = np.empty((T_test.shape[0],10))
 
-for i in range(y.shape[1]):
+for i in range(y_train.shape[1]):
     clf = SGDClassifier()
 
     # select 61 best feature columns
     selector = SelectKBest(k=61)
-    X_new = selector.fit_transform(X_train, y[:,i])
+    X_new = selector.fit_transform(X_train, y_train[:,i])
 
     # apply mask to get the selected columns from T
     mask = selector.get_support()
-    T_new = T[:,mask]
+    T_new = T_test[:,mask]
 
-    clf.fit(X_new, y[:,i])
+    clf.fit(X_new, y_train[:,i])
     coef = clf.coef_
     coef = coef.reshape((coef.shape[1],))
 
     # calculate sigmoid to get probabilities in range [0,1]
     output[:,i] = 1/(1 + np.exp(-np.dot(T_new,coef)))
 
-pids = test_df.to_numpy()[::12,0]
+pids = df_test.to_numpy()[::12,0]
 pids = pids.reshape((len(pids),1))
 output = np.hstack((pids, output))
-df = pd.DataFrame(output,columns=label_df.columns)
+df = pd.DataFrame(output,columns=df_label.columns)
 df = df.astype({'pid':'int32'})
 df.to_csv('Data/pred_st1_Sven.csv', index=False, float_format='%.3f',header=True)
 
