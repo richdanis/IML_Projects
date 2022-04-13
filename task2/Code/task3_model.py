e54115d02b4ba8f453e160ea2b1b20eb776913fc
Richard Danis <rdanis@ethz.ch>
2022-04-13 21:00:55 +0200

    Removed outlier detection from task3

--- task2/Code/task3_model.py
+++ task2/Code/task3_model.py
@@ -2,22 +2,16 @@ import numpy as np
 import pandas as pd
 from sklearn.linear_model import SGDRegressor
 import helper_functions as hf
-from sklearn.preprocessing import PolynomialFeatures
 
 fname = "Data/"
 train_df = pd.read_csv(fname + "train_features.csv")
 label_df = pd.read_csv(fname + "train_labels.csv")
 test_df = pd.read_csv(fname + "test_features.csv")
 
-columns = ["pid","Time","Age","RRate","ABPm","SpO2","Heartrate"]
-
-train_df = train_df[columns]
-test_df = test_df[columns]
-
-train_df, label_df = hf.remove_outliers(train_df,label_df)
-
-X = hf.take_mean_features(train_df,["Time","Age"])
-T = hf.take_mean_features(test_df,["Time","Age"])
+X = hf.take_mean_features(train_df,["Time"])
+T = hf.take_mean_features(test_df,["Time"])
+X = hf.normalize(X)
+T = hf.normalize(T)
 
 columns = ["LABEL_RRate","LABEL_ABPm","LABEL_SpO2","LABEL_Heartrate"]
 
@@ -28,21 +22,10 @@ y = label_df[columns].to_numpy()
 
 output = np.empty((pids.shape[0],4))
 
-p = [5, 7, 1, 8]
-
 for i in range(4):
     reg = SGDRegressor()
-    poly = PolynomialFeatures(p[i])
-    X_new = X[:,i]
-    T_new = T[:,i]
-    X_new = X_new.reshape((X_new.shape[0],1))
-    T_new = T_new.reshape((T_new.shape[0], 1))
-    X_new = poly.fit_transform(X_new)
-    T_new = poly.fit_transform(T_new)
-    X_new = hf.normalize(X_new)
-    T_new = hf.normalize(T_new)
-    reg.fit(X_new,y[:,i])
-    output[:,i] = reg.predict(T_new)
+    reg.fit(X,y[:,i])
+    output[:,i] = reg.predict(T)
 
 output = np.hstack((pids, output))
 df = pd.DataFrame(output,columns=["pid"] + columns)
--- task2/Code/task3_validation.py
+++ task2/Code/task3_validation.py
@@ -1,42 +1,28 @@
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import SGDRegressor
-from sklearn.feature_selection import SelectKBest, f_regression
 import helper_functions as hf
 from sklearn.model_selection import cross_val_score
-from sklearn.preprocessing import PolynomialFeatures
 
 fname = "Data/"
 train_df = pd.read_csv(fname + "train_features.csv")
 label_df = pd.read_csv(fname + "train_labels.csv")
-test_df = pd.read_csv(fname + "test_features.csv")
 
-columns = ["pid","Time","Age","RRate","ABPm","SpO2","Heartrate"]
-
-train_df = train_df[columns]
-
-train_df, label_df = hf.remove_outliers(train_df, label_df)
-
-X = hf.take_mean_features(train_df,["Time","Age"])
+X = hf.take_mean_features(train_df,["Time"])
+X = hf.normalize(X)
 
 columns = ["LABEL_RRate","LABEL_ABPm","LABEL_SpO2","LABEL_Heartrate"]
 
 y = label_df[columns].to_numpy()
-# found out these polynomial degrees through trial and error
-p = [5, 7, 1, 8]
+
 for i in range(4):
     reg = SGDRegressor()
-    poly = PolynomialFeatures(p[i])
-    X_new = X[:,i]
-    X_new = X_new.reshape((X.shape[0],1))
-    X_new = poly.fit_transform(X_new)
-    X_new = hf.normalize(X_new)
-    scores = cross_val_score(reg, X_new, y[:, i], cv=10)
+    scores = cross_val_score(reg, X, y[:, i], cv=10)
     print("Score: ",np.mean(scores))
 
 '''
-Score:  0.1355936387732553
-Score:  0.30412520112397556
-Score:  0.3538507248387641
-Score:  0.26148310164533284
+Score:  0.37592154179722764
+Score:  0.5728583008037672
+Score:  0.2578932417363745
+Score:  0.596710935341267
 '''
