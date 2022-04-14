3d75931fbf4a816bab7e473e93c8fe0f0cc8cf16
Sven Gutjahr <sven.gutjahr@yahoo.de>
2022-04-14 10:14:41 +0200

    10141404

--- task2/Code/task1_model_Sven.py
+++ task2/Code/task1_model_Sven.py
@@ -1,11 +1,16 @@
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import SGDClassifier
-from sklearn.model_selection import cross_val_score
 from sklearn.feature_selection import SelectKBest
 
+
+# Defining the columns needed in Subtask1
 TEST1 = ['LABEL_BaseExcess', 'LABEL_Fibrinogen', 'LABEL_AST', 'LABEL_Alkalinephos', 'LABEL_Bilirubin_total',
          'LABEL_Lactate', 'LABEL_TroponinI', 'LABEL_SaO2', 'LABEL_Bilirubin_direct', 'LABEL_EtCO2']
+
+SOL1= ['pid','LABEL_BaseExcess', 'LABEL_Fibrinogen', 'LABEL_AST', 'LABEL_Alkalinephos', 'LABEL_Bilirubin_total',
+         'LABEL_Lactate', 'LABEL_TroponinI', 'LABEL_SaO2', 'LABEL_Bilirubin_direct', 'LABEL_EtCO2']
+
 
 def remove_outliers(features,labels):
     for col in features.columns:
@@ -27,51 +32,55 @@ def remove_outliers(features,labels):
 
     return features, labels
 
+# Normalizing and retun a np matrix
 def prepare_dataset(df):
-    df = df.drop('pids')
+    df = df.drop(columns='pid')
     df = (df - df.mean()) / df.std()
 
     X = df.to_numpy()
     return X
 
+# Import the data
 fname = 'Data/'
 df_train = pd.read_csv(fname + 'train_features_Sven_short.csv')
 df_label = pd.read_csv(fname + 'train_labels_sorted.csv')
 df_test = pd.read_csv(fname + 'test_features_Sven_short.csv')
 
+#df -> np matrix
 X_train = prepare_dataset(df_train)
 T_test = prepare_dataset(df_test)
 
-# PREPARING TRAINING LABELS
-df_label = df_label.sort_values(by=['pid'])
-df_label = df_label[TEST1]
+# Labels for Subtask1_1 -> np matrix
+df_subtaskt_1 = df_label[TEST1]
+y_train = df_subtaskt_1.to_numpy()
 
-y_train = df_label.to_numpy()
-
+# prediction matrix 
 output = np.empty((T_test.shape[0],10))
+
 
 for i in range(y_train.shape[1]):
     clf = SGDClassifier()
 
-    # select 61 best feature columns
-    selector = SelectKBest(k=61)
-    X_new = selector.fit_transform(X_train, y_train[:,i])
+    # # select 61 best feature columns
+    #selector = SelectKBest(k=61)
+    #X_new = selector.fit_transform(X_train, y_train[:,i])
 
     # apply mask to get the selected columns from T
-    mask = selector.get_support()
-    T_new = T_test[:,mask]
+    # mask = selector.get_support()
+    # T_new = T_test[:,mask]
 
-    clf.fit(X_new, y_train[:,i])
+    clf.fit(X_train, y_train[:,i])
     coef = clf.coef_
     coef = coef.reshape((coef.shape[1],))
 
     # calculate sigmoid to get probabilities in range [0,1]
-    output[:,i] = 1/(1 + np.exp(-np.dot(T_new,coef)))
+    output[:,i] = 1/(1 + np.exp(-np.dot(T_test,coef)))
+    
 
-pids = df_test.to_numpy()[::12,0]
+pids = df_test['pid'].to_numpy()
 pids = pids.reshape((len(pids),1))
 output = np.hstack((pids, output))
-df = pd.DataFrame(output,columns=df_label.columns)
+df = pd.DataFrame(output,columns=SOL1)
 df = df.astype({'pid':'int32'})
 df.to_csv('Data/pred_st1_Sven.csv', index=False, float_format='%.3f',header=True)
 
