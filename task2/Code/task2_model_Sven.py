410e5fa9759decf382f026b046674f996bc58f40
Sven Gutjahr <sven.gutjahr@yahoo.de>
2022-04-15 13:47:20 +0200

    13471504

--- task2/Code/task2_model_Sven.py
+++ task2/Code/task2_model_Sven.py
@@ -19,8 +19,8 @@ SOL  =  ['pid', 'LABEL_Sepsis']
 fname = 'Data/'
 df_train = pd.read_csv(fname + 'train_features_Sven_long.csv')
 df_label = pd.read_csv(fname + 'train_labels_sorted.csv')
-#df_test = df_train.copy()
-df_test = pd.read_csv(fname + 'test_features_Sven_long.csv')
+df_test = df_train.copy()
+#df_test = pd.read_csv(fname + 'test_features_Sven_long.csv')
 df_true = df_label.copy()
 
 #df -> np matrix
@@ -31,8 +31,6 @@ poly = PolynomialFeatures(2)
 X_train = poly.fit_transform(X_train)
 T_test = poly.fit_transform(T_test)
 
-print(X_train.shape)
-print(T_test.shape)
 # Labels for Subtask1_1 -> np matrix
 df_subtask_2 = df_label[TEST2]
 y_train_2 = df_subtask_2.to_numpy()
@@ -45,10 +43,11 @@ print(f'Subtask2: {TEST2[0]}')
 #clf = LogisticRegression(penalty='l1',max_iter=1000,solver='liblinear')
 clf = LogisticRegression(max_iter=3000)
 clf.fit(X_train, y_train_2)
-coef = clf.coef_
-coef = coef.reshape((coef.shape[1],))
-pred_2 = hf.sigmoid(T_test,coef)
-
+#coef = clf.coef_
+#coef = coef.reshape((coef.shape[1],))
+pred_2 = clf.predict_proba(T_test)[:,1]
+#pred_2 = hf.sigmoid(T_test,coef)
+print(pred_2.shape)
 pids = df_test['pid'].to_numpy()[::12]
 
 pred = np.empty((T_test.shape[0],2))
@@ -58,6 +57,6 @@ pred[:,1] = pred_2
 df = pd.DataFrame(pred,columns=SOL)
 df = df.astype({'pid':'int32'}) 
 
-#score = hf.get_score(df_true, df, False, True)
+score = hf.get_score(df_true, df, False, True)
 
-df.to_csv('Data/pred_st2_Sven.csv', index=False, float_format='%.3f',header=True)
+#df.to_csv('Data/pred_st2_Sven.csv', index=False, float_format='%.3f',header=True)
