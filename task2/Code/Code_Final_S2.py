7733244e491aa7dcd2a46e4cf15ae886e3f953d5
Sven Gutjahr <sven.gutjahr@yahoo.de>
2022-04-24 19:47:16 +0200

    Done!

---
+++ task2/Code/Code_Final_S2.py
@@ -0,0 +1,124 @@
+#imports
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import GradientBoostingRegressor
+import helper_functions_Sven as hf
+
+#Score in ST1: 0.8607528901743648
+#Score in ST2: 0.8127179099776952
+#Score in ST3: 0.7874488604012626
+#Score: 0.8203065535177743
+
+# label for the Subtasks 1-3
+TASK1 = ['LABEL_BaseExcess', 'LABEL_Fibrinogen', 'LABEL_AST', 'LABEL_Alkalinephos', 'LABEL_Bilirubin_total', 'LABEL_Lactate',
+         'LABEL_TroponinI', 'LABEL_SaO2', 'LABEL_Bilirubin_direct', 'LABEL_EtCO2']
+TASK2 = ['LABEL_Sepsis']
+TASK3 = ['LABEL_RRate', 'LABEL_ABPm', 'LABEL_SpO2', 'LABEL_Heartrate']
+
+# drop pid, time and age column
+# then for each patient, take the min, mean, max and last of all feature columns
+# results in 34 * 4 = 136 features per patient
+# add age back, 137 features per patient
+
+def normalise(X):
+    mean = np.mean(X,axis=0)
+    mean = np.resize(mean,(1,mean.shape[0]))
+    std = np.std(X,axis=0)
+    std = np.resize(std,(1,std.shape[0]))
+    X = (X-mean)/std
+    return X
+
+def min_mean_max_last(df):
+    
+    df = df.drop(columns=['pid'])
+    if 'Time' in df.columns:
+        df = df.drop(columns=['Time'])
+
+    X = df.to_numpy()
+    # exclude pids age and time
+    age = X[::12, 0]
+    age = age.reshape((len(age), 1))
+    X = X[:, 1:]
+
+    ds = np.empty((X.shape[0]//12,X.shape[1]*4))
+
+    for i in range(ds.shape[0]):
+        mean = X[(i*12):((i+1)*12)].mean(axis=0)
+        min = X[(i*12):((i+1)*12)].min(axis=0)
+        max = X[(i*12):((i+1)*12)].max(axis=0)
+        last = X[(i+1) * 12 - 1]
+        ds[i,:X.shape[1]] = mean
+        ds[i,X.shape[1]:2*X.shape[1]] = min
+        ds[i,2*X.shape[1]:3*X.shape[1]] = max
+        ds[i,3*X.shape[1]:] = last
+
+    #ds = normalise(ds)
+    
+    ds = np.hstack((age,ds))
+    return ds
+
+fname = "Data/"
+df_train = pd.read_csv(fname + "train_features.csv")
+df_train = df_train.fillna(df_train.mean())
+#df_train = df_train.fillna(0)
+df_label = pd.read_csv(fname + "train_labels.csv")
+#df_test = df_train.copy()
+#df_true = df_label
+df_test = pd.read_csv(fname + "test_features.csv")
+df_test = df_test.fillna(df_test.mean())
+#df_test = df_test.fillna(0)
+
+# Preprocessing the data
+# mmml + norm
+X = min_mean_max_last(df_train)
+T = min_mean_max_last(df_test)
+
+#Setting the y to the Subtasks
+y1 = df_label[TASK1].to_numpy()
+y2 = df_label[TASK2].to_numpy()
+y2 = np.ravel(y2)
+y3 = df_label[TASK3].to_numpy()
+
+GBC = GradientBoostingClassifier(random_state=12)
+GBR = GradientBoostingRegressor(random_state=9)
+
+# TASK 1
+print(f'Subtask 1')
+pred_1 = np.empty((T.shape[0],len(TASK1)))
+
+for i in range(y1.shape[1]):
+    print(f'Round {i+1}: {TASK1[i]}')
+    GBC.fit(X, y1[:, i])
+    pred_1[:, i] = GBC.predict_proba(T)[:, 1]
+
+# TASK 2
+print(f'Subtask 2: {TASK2[0]}')
+pred_2 = np.empty((T.shape[0],len(TASK2)))
+
+GBC.fit(X,y2)
+pred_2 = GBC.predict_proba(T)[:,1]
+pred_2 = pred_2.reshape((pred_2.shape[0],1))
+
+# TASK 3
+print(f'Subtask 3')
+pred_3 = np.empty((T.shape[0],len(TASK3)))
+
+for i in range(y3.shape[1]):
+    print(f'Round {i+1}: {TASK3[i]}')
+    GBR.fit(X, y3[:, i])
+    pred_3[:, i] = GBR.predict(T)
+    
+# Merging the predictions of the subtask and saving to csv
+pids = df_test["pid"].to_numpy()[::12]
+pids = pids.reshape((len(pids),1))
+
+final = np.hstack((pred_1,pred_2))
+final = np.hstack((final,pred_3))
+final = np.hstack((pids,final))
+
+sub = pd.DataFrame(final,columns=['pid'] + TASK1 + TASK2 + TASK3)
+sub = sub.astype({'pid':'int32'})
+
+#hf.get_score(df_true, sub)
+sub.to_csv('Data/Sub_SvGi_mean.csv', index=False, float_format='%.3f', header=True)
--- task2/Code/helper_functions_Sven.py
+++ task2/Code/helper_functions_Sven.py
@@ -5,17 +5,20 @@ import sklearn.metrics as metrics
 TEST1 = ['LABEL_BaseExcess', 'LABEL_Fibrinogen', 'LABEL_AST', 'LABEL_Alkalinephos', 'LABEL_Bilirubin_total',
          'LABEL_Lactate', 'LABEL_TroponinI', 'LABEL_SaO2', 'LABEL_Bilirubin_direct', 'LABEL_EtCO2']
 TEST2 = ['LABEL_Sepsis']
+TEST3 = ['LABEL_RRate', 'LABEL_ABPm', 'LABEL_SpO2', 'LABEL_Heartrate']
 
 #get score
-def get_score(df_true, df_submission, T1 = False, T2 = False):
+def get_score(df_true, df_submission):
     df_submission = df_submission.sort_values('pid')
     df_true = df_true.sort_values('pid')
-    if T1:
-        task1 = np.mean([metrics.roc_auc_score(df_true[entry], df_submission[entry]) for entry in TEST1])
-        print(f'Score in ST1: {task1}')
-    if T2:
-        task2 = metrics.roc_auc_score(df_true[TEST2[0]], df_submission[TEST2[0]])
-        print(f'Score in ST2: {task2}')
+    task1, task2, task3, = 0,0,0
+    task1 = np.mean([metrics.roc_auc_score(df_true[entry], df_submission[entry]) for entry in TEST1])
+    print(f'Score in ST1: {task1}')
+    task2 = metrics.roc_auc_score(df_true[TEST2[0]], df_submission[TEST2[0]])
+    print(f'Score in ST2: {task2}')
+    task3 = np.mean([0.5 + 0.5 * np.maximum(0, metrics.r2_score(df_true[entry], df_submission[entry])) for entry in TEST3])
+    print(f'Score in ST3: {task3}')
+    print(f'Score: {np.mean([task1, task2, task3])}')
     return
 
 # Sigmoind
