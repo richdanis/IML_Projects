260ab55c149fa945d94511e9c5b0abb756dfe537
Sven Gutjahr <sven.gutjahr@yahoo.de>
2022-04-26 16:44:09 +0200

    task3

--- task3/Sven.py
+++ task3/Sven.py
@@ -1,1 +1,12 @@
-print('hello')
+#imports
+import numpy as np
+#import pytorch as torch
+
+#data
+fname = 'Data/'
+food = fname + 'food/'
+train = np.loadtxt(fname + "train_triplets.txt", dtype=int)
+#test = np.loadtxt(fname + "test_triplets.txt", dtype=int)
+
+
+
