2e9202d76d317661677c55453ef4a0965e761478
Richard Danis <rdanis@ethz.ch>
2022-04-26 18:28:59 +0200

    TensorToImage() function

--- task3/Code/Richard.py
+++ task3/Code/Richard.py
@@ -1,15 +1,39 @@
 import torch
 
-#imports
 import numpy as np
-#import pytorch as torch
+from torchvision import transforms
+from PIL import Image
+
+def ImageToTensor(img):
+
+    filename = '../Data/food/' + str(img) + '.jpg'
+    image = Image.open(filename)
+    to_tensor = transforms.ToTensor()
+    tensor = to_tensor(image)
+    tensor = tensor.resize((3,))
+
+    return tensor
+
 
 #data
-fname = 'Data/'
+fname = '../Data/'
 food = fname + 'food/'
-train = np.loadtxt(fname + "train_triplets.txt", dtype=int)
+train = np.loadtxt(fname + "train_triplets.txt", dtype=str)
 #test = np.loadtxt(fname + "test_triplets.txt", dtype=int)
 print("hey")
+
+filename = '../Data/food/' + str(train[0][0]) + '.jpg'
+img = Image.open(filename)
+to_tensor = transforms.ToTensor()
+tensor = to_tensor(img)
+
+filename = '../Data/food/' + str(train[0][1]) + '.jpg'
+img = Image.open(filename)
+to_tensor = transforms.ToTensor()
+tensor = to_tensor(img)
+print("hehe")
+
+
 
 
 
