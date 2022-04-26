a1e4f687c0823b4ca5fa58162bc36412f4fe8985
Sven Gutjahr <sven.gutjahr@yahoo.de>
2022-04-26 17:10:41 +0200

    Swap

--- task3/Sven.py
+++ task3/Sven.py
@@ -1,5 +1,6 @@
 #imports
 import numpy as np
+from numpy import random as ran
 #import pytorch as torch
 
 #data
@@ -7,6 +8,14 @@ fname = 'Data/'
 food = fname + 'food/'
 train = np.loadtxt(fname + "train_triplets.txt", dtype=int)
 #test = np.loadtxt(fname + "test_triplets.txt", dtype=int)
+print(train)
 
-
-
+y = ran.choice(a=[0, 1], size= (train.shape[0],1))
+print(y)
+for i in range(train.shape[0]):
+    if y[i] == 0:
+        tmp = train[i,1]
+        train[i,1] = train[i,2]
+        train[i,2] = tmp
+        
+print(train)
