1da5a1209b2ba6908b36fe1fffcc84aeb607f45c
Richard Danis <rdanis@ethz.ch>
2022-04-26 17:14:45 +0200

    Richard python first file

---
+++ task3/Code/Richard.py
@@ -0,0 +1,15 @@
+import torch
+
+#imports
+import numpy as np
+#import pytorch as torch
+
+#data
+fname = 'Data/'
+food = fname + 'food/'
+train = np.loadtxt(fname + "train_triplets.txt", dtype=int)
+#test = np.loadtxt(fname + "test_triplets.txt", dtype=int)
+print("hey")
+
+
+
