5b6bbd08a9b2da4e669f43b4c82b101980a45282
Sven Gutjahr <sven.gutjahr@yahoo.de>
2022-04-28 11:55:13 +0200

    Sven versucht was

--- task3/Code/Sven.py
+++ task3/Code/Sven.py
@@ -4,17 +4,19 @@ from numpy import random as ran
 import help_Sven as h
 import torch
 
-    
 #data
 fname = '../Data/'
 food = fname + 'food/'
 train = np.loadtxt(fname + "train_triplets.txt", dtype=str)
 #test = np.loadtxt(fname + "test_triplets.txt", dtype=int)
 
-
 y, train = h.swap(train)
 i = 1000
+
 A,B,C = h.get_ima(train,i)
+
 print(A.shape)
 print(B.shape)
 print(C.shape)
+
+print('Hello World!')
